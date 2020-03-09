f0ff6dba6b143144fdf3e2074ea74b344925f2af
yunaganyh <43854098+yunaganyh@users.noreply.github.com>
2020-03-08 23:29:39 -0400

    done part 1 and 2, question on test for 1

--- s1c01.py
+++ s1c01.py
@@ -1,7 +1,11 @@
-
+import binascii as ba
 
 def b64ToHex(b64String):
-    pass
+    binary = ba.a2b_base64(b64String)
+    convertedToHex = ba.b2a_hex(binary)
+    return convertedToHex
 
 def hexToB64(hexString):
-    pass
+    binary = ba.a2b_hex(hexString)
+    convertedToB64 = ba.b2a_base64(binary, newline=False)
+    return convertedToB64
--- s1c02.py
+++ s1c02.py
@@ -1,3 +1,17 @@
+import s1c01
+import binascii as ba
+
+def hexToBinary(hexString):
+    return ba.a2b_hex(hexString)
+
+def convertToBytes(string):
+    return bytes(string, 'utf-8')
 
 def xor(s1, s2):
-    pass
+    s3 = b''
+    b1 = hexToBinary(s1)
+    b2 = hexToBinary(s2)
+    for (i,j) in zip(b1,b2):
+        xor = convertToBytes('{0:02x}'.format(int(hex(i^j), 16)))
+        s3 += xor
+    return s3
